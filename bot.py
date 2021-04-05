dbefeee5eb8adc50ac0ebaf98d09c4628aabe8e2
Andrey <andrei.krotkih@gmail.com>
2021-04-05 15:51:03 +0500

    Тренировка на сервер FX42

--- bot.py
+++ bot.py
@@ -18,6 +18,70 @@ import re
 import sys
 import random
 
+class neuralNetFX42():
+
+    def __init__(self):
+
+        ## model load
+
+        self.maxlen = 120
+        with open('chars_list_FX42.data', 'rb') as filehandle:
+            # read the data as binary data stream
+            self.chars = pickle.load(filehandle)
+        print('Unique characters:', len(self.chars))
+
+        print(self.chars)
+
+        self.char_indices = dict(((char, self.chars.index(char)) for char in self.chars))
+
+        checkpoint_path = "training_FX42_V2/cp.ckpt"
+
+        self.model3 = Sequential()
+        self.model3.add(LSTM(256, input_shape=(self.maxlen, len(self.chars)), return_sequences=True))
+        self.model3.add(LSTM(256, input_shape=(self.maxlen, len(self.chars)), return_sequences=True))
+        self.model3.add(LSTM(256, input_shape=(self.maxlen, len(self.chars)), return_sequences=True))
+        self.model3.add(LSTM(256, input_shape=(self.maxlen, len(self.chars)), return_sequences=True))
+        self.model3.add(Flatten())
+        self.model3.add(Dense(len(self.chars), activation='softmax'))
+        self.model3.compile(optimizer='rmsprop', loss='categorical_crossentropy')
+        self.model3.load_weights(checkpoint_path)
+        print('modelFX42 on')
+
+    def sample(self, preds, temperature):
+        preds = numpy.asarray(preds).astype('float64')
+        #preds = preds.clip(min=0.0000000000000001)
+        preds = numpy.log(preds) / temperature
+        exp_preds = numpy.exp(preds)
+        preds = exp_preds / numpy.sum(exp_preds)
+        probas = numpy.random.multinomial(1, preds, 1)
+        return numpy.argmax(probas)
+
+    def continue_msg(self, msg, temperature, msglength):
+        generated_text = msg[:self.maxlen]
+        result = ''
+        #print(len(generated_text))
+        if len(generated_text) >= self.maxlen:
+            sys.stdout.write(result)
+            for i in range(msglength):
+                sampled = numpy.zeros((1, self.maxlen, len(self.chars)))
+                for t, char in enumerate(generated_text):
+
+
+                    #print('+ ', str(char))
+                    sampled[0, t, self.char_indices[char]] = 1.
+                    #print('+ ', sampled)
+                preds = self.model3.predict(sampled, verbose=0)[0]
+                next_index = self.sample(preds, temperature)
+                next_char = self.chars[next_index]
+                #print(next_index)
+                generated_text += next_char
+                result += next_char
+                generated_text = generated_text[1:]
+                sys.stdout.write(next_char)
+            print(result)
+            return result
+        else:
+            return ('Системное сообщение: Мало текста для ответа')
 
 class neuralNet():
 
@@ -158,9 +222,9 @@ class neuralNet():
     def continue_msg(self, msg, temperature, msglength):
         generated_text = msg[:60]
         result = ''
+        print(len(generated_text))
         if len(generated_text) >= 60:
-            print(len(generated_text))
-            sys.stdout.write(generated_text)
+            sys.stdout.write(result)
             for i in range(msglength):
                 sampled = numpy.zeros((1, self.maxlen, len(self.chars)))
                 for t, char in enumerate(generated_text):
@@ -183,7 +247,6 @@ class neuralNet():
         else:
             return ('Системное сообщение: Мало текста для ответа')
 
-
 class MyClient(discord.Client):
 
     async def on_ready(self):
@@ -194,26 +257,24 @@ class MyClient(discord.Client):
     async def on_message(self, message):
         # print(message.author.name)
         # print(message.author.id)
-        if False:#not(message.content.startswith('++')) and str(message.channel) == 'урал-вагон-завод':
+        # Версия бота
+        if message.content.startswith('++version'):
             channel = message.channel
-            train_data = open("train.txt", "a")
-            try:
-                train_data.write(str(message.author.name) + "\\" + message.content + " \n")
-            except:
-                print("Caught exception")
-            train_data.close()
+            embedding = discord.Embed()
+            embedding.title = 'Giallar Replication Neural Net'
+            embedding.add_field(name='CPU', value='Intel(R) Xeon(R) CPU @ 2.20GHz', inline=False)
+            embedding.add_field(name='GPU', value='Nvidia Tesla T4', inline=False)
+            embedding.add_field(name='Version', value='0.1', inline=False)
+            embedding.add_field(name='Нейронная сеть определяющая контекст', value='Off', inline=False)
+            embedding.add_field(name='Нейронная сеть определяющая длину сообщения', value='Off', inline=False)
+            embedding.add_field(name='Нейронная сеть определяющая необходимость ответа', value='Off', inline=False)
+            embedding.add_field(name='Нейронная сеть NLP', value='On', inline=False)
+            embedding.add_field(name='Чего ждать', value='На текущий момент это просто генератор продолжения текста на основе последних 100 предложений в чате', inline=False)
+            embedding.add_field(name='Текущий объём данных для RADIANCE', value='10% от сообщений Giallar\'a чата урал-вагон-завод', inline=False)
+            embedding.add_field(name='Текущий объём данных для FX42', value='20% от сообщений основного чата', inline=False)
+            #embedding.description = 'Реализует нейронную сеть обучающуюся на примере общения одного пользователя.'
 
-            if str(message.channel) == 'урал-вагон-завод':
-                print(neural.classify(str(message.content)))
-            # await channel.send(content=message.content)
-
-        if False: #message.content.startswith('++classify ') and message.author.id == 366921098077667338:
-            channel = message.channel
-            print(message.content[11:])
-            if len(message.content[11:].split(' ')) <= 4:
-                await channel.send(content='Малое количество слов в последовательности')
-            else:
-                await channel.send(content=neural.classify(str(message.content[11:])))
+            await channel.send(embed=embedding)
 
         if message.content.startswith('++загрузитьисторию') and message.author.id == 366921098077667338:
             channel = message.channel
@@ -222,7 +283,7 @@ class MyClient(discord.Client):
             startTime = time.time()
             messages = await channel.history(limit=None).flatten()
 
-            train_data = open("train3.txt", "a", errors="surrogateescape")
+            train_data = open("train.txt" + str(message.channel.name), "a", errors="surrogateescape")
             for msg in reversed(messages):
                 try:
 
@@ -248,14 +309,14 @@ class MyClient(discord.Client):
             for msg in messages:
                 msglist = numpy.append(msglist, msg.content)
             print('\n Тест ответа \n')
-            joined_msg = ' '.join(msglist)
+            joined_msg = ' '.join(reversed(msglist))
             test = map(str.strip, str.split(''.join(filter(lambda x: x.isalnum or x == ' ', joined_msg)), '\n'))
             test = ' '.join([str(x) for x in test])
             joined_msg = re.sub(r'<[^()]*>', '', test)
             #print(joined_msg.lower())
             messagelen = random.randint(50, 100)
             print(messagelen)
-            print(len(joined_msg.lower()))
+            #print(len(joined_msg.lower()))
             if len(joined_msg.lower()) >= 60:
                 await channel.send(content=neural.continue_msg(joined_msg.lower(), 0.2, messagelen))
 
@@ -279,9 +340,9 @@ class MyClient(discord.Client):
                 print(str(check))
                 chance_generator = random.random()
                 print(chance_generator)
+                joined_msg = ' '.join(reversed(msglist))
+                print(joined_msg[-60:])
                 if chance_generator > 0.9:
-                    print('\n Тест ответа \n')
-                    joined_msg = ' '.join(msglist)
                     test = map(str.strip, str.split(''.join(filter(lambda x: x.isalnum or x == ' ', joined_msg)), '\n'))
                     test = ' '.join([str(x) for x in test])
                     joined_msg = re.sub(r'<[^()]*>', '', test)
@@ -289,28 +350,38 @@ class MyClient(discord.Client):
                     messagelen = random.randint(50, 100)
                     print(messagelen)
                     if len(joined_msg.lower()) >= 60:
-                        await channel.send(content=neural.continue_msg(joined_msg.lower(), 0.2, messagelen))
+                        resultmessage = neural.continue_msg(joined_msg.lower(), 0.2, messagelen)
+                        newmessage = ' '.join(resultmessage.split(' ')[1:-1])
+                        await channel.send(content=newmessage)
                     #print(neural.continue_msg(joined_msg.lower(), 1.))
 
-        if message.content.startswith('++generate') and message.author.id == 366921098077667338:
+        #FX42
+        if message.channel.id == 775119952344514590:
             channel = message.channel
-            messages = await channel.history(limit=50).flatten()
+            messages = await channel.history(limit=100).flatten()
             msglist = numpy.array([])
             for msg in messages:
                 msglist = numpy.append(msglist, msg.content)
-            print('\n Тест ответа \n')
-            joined_msg = ' '.join(msglist)
-            test = map(str.strip, str.split(''.join(filter(lambda x: x.isalnum or x == ' ', joined_msg)), '\n'))
-            test = ' '.join([str(x) for x in test])
-            joined_msg = re.sub(r'<[^()]*>', '', test)
-            #print(joined_msg.lower())
-            messagelen = random.randint(100, 150)
-            print(messagelen, ' ', len(joined_msg.lower() ))
-            if len(joined_msg.lower()) >= 60:
-                await channel.send(content=neural.continue_msg(joined_msg.lower(), 0.1, messagelen))
+            chance_generator = random.random()
+            joined_msg = ' '.join(reversed(msglist))
+            print('+')
+            print(chance_generator)
+            if chance_generator > 0.8:
+                test = map(str.strip, str.split(''.join(filter(lambda x: x.isalnum or x == ' ', joined_msg)), '\n'))
+                test = ' '.join([str(x) for x in test])
+                joined_msg = re.sub(r'<[^()]*>', '', test)
+                #print(joined_msg.lower())
+                messagelen = random.randint(110, 120)
+                print('Длина сообщения ', len(joined_msg.lower()))
+                if len(joined_msg.lower()) >= neuralFX42.maxlen:
+                    resultmessage = neuralFX42.continue_msg(joined_msg.lower(), 0.2, messagelen)
+                    newmessage = ' '.join(resultmessage.split(' ')[1:-1])
+                    await channel.send(content=newmessage)
+                #print(neural.continue_msg(joined_msg.lower(), 1.))
 
         #print('Message from {0.author} on {0.guild} on {0.channel}: {0.content}'.format(message))
 
 neural = neuralNet()
+neuralFX42 = neuralNetFX42()
 client = MyClient()
-client.run('NTg5NDQxOTgzNTA5MTY4MTI4.XQTuoA.n8SbAoYYnb4YX0OmKfQcwJyZG2U')
+client.run('NTg5NDQxOTgzNTA5MTY4MTI4.XQTuoA.qrzBiNAAdjg1psy7mhtjvCFMqik')
